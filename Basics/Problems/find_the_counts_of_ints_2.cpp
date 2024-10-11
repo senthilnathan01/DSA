cd08230880e6116776050ded5e6b1c8e91452eae
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2024-10-11 19:27:11 +0530

    Add files via upload

---
+++ Basics/Problems/find_the_counts_of_ints_2.cpp
@@ -0,0 +1,28 @@
+// #include<bits/stdc++.h> or alternatively use this 
+#include<iostream>
+#include<map>
+
+using namespace std;
+
+// Problem:
+// Find the number of times the numbers: 1, 4, 2, 3, 12 appear in the
+// array: {1, 3, 2, 1, 3}
+
+// Previously we saw how to use hashing to solve this problem
+// Let's solve this using mapping
+
+int main(){
+    int arr[5] = {1, 3, 2, 1, 3};
+    map<int, int> mpp;
+    for(int i=0; i<5; i++){
+        mpp[arr[i]]++;
+    }
+
+    int find_counts[5]={1, 4, 2, 3, 12};
+    for(int i=0; i<5; i++){
+        cout<<mpp[find_counts[i]]<<" ";
+    }
+    return 0;
+}
+
+// Output: 2 0 1 2 0
