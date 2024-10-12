68ecf1d0f1b967bf785ba0221632f10c5f3edafb
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2024-10-12 18:04:31 +0530

    Update largest_in_an_array.cpp

--- Basics/Problems/Arrays/largest_in_an_array.cpp
+++ Basics/Problems/Arrays/largest_in_an_array.cpp
@@ -3,6 +3,14 @@
 
 using namespace std;
 
+// Brute force approach will be to sort the array and return the last element in the array as the largest element
+// Time complexity = O(nlogn)
+
+// Optimal Approach
+// Just traverse the array and keep updating the largest element if you find any element larger than the one we assume
+// Time complexity = O(n)
+
+// Here I used a STL for a much faster solution
 // Finds the largest element in an array.
 int find_largest_element(int arr[], int size) {
     return *max_element(arr, arr + size);
