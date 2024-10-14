aa44231b42657eb59d17292e5c6d29ada2e60838
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2024-10-14 17:28:30 +0530

    Create binary_search_in_array.cpp

---
+++ Basics/Problems/Binary_Search/binary_search_in_array.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+/*
+Given an array arr[] sorted in ascending order of size N and an integer K. 
+Check if K is present in the array or not.
+*/
+
+// We use binary search to do this task (mainly because the array is already sorted).
+// Time complexity = O(logn)
+
+// Whereas if you use linear search the time complexity will O(n)
+
+int binary_search(int arr[], int low, int high, int K){
+    if(low<=high){
+        int mid = low + (high-low)/2;
+        if(K==arr[mid]){
+            return mid;
+        }
+        else if(K<arr[mid]){
+            return binary_search(arr, low, mid-1, K);
+        }
+        else{
+            return binary_search(arr, mid+1, high, K);
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int nums[8] = {1, 2, 0, 3, 0, 4, 0, 5};
+    return binary_search(nums, 0, 8, 3);
+}
