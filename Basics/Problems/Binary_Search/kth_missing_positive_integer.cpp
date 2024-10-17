fd326d4812de316be641c2ad9ca906fc2545ab44
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2024-10-17 22:14:22 +0530

    Update kth_missing_positive_integer.cpp

--- Basics/Problems/Binary_Search/kth_missing_positive_integer.cpp
+++ Basics/Problems/Binary_Search/kth_missing_positive_integer.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include<vector>
-#include<algorithm>
 
 using namespace std;
 
@@ -9,7 +8,11 @@ Given an array arr of positive integers sorted in a strictly increasing order, a
 
 Return the kth positive integer that is missing from this array.
 */
-int findKthPositive(vector<int>& arr, int k) {
+
+// Approach 1:
+// TC = O(n)
+
+int findKthPositive1(vector<int>& arr, int k) {
     int i = 1;  // Current positive integer
     int j = 0;  // Index in the array
     int size = arr.size();
@@ -33,11 +36,105 @@ int findKthPositive(vector<int>& arr, int k) {
     return i - 1;
 }
 
+// Approach 2: Using binary Search
+// TC = log(n)
+
+int findKthPositive2(vector<int>& arr, int k){
+    if(arr[0]>k) return k;
+
+    int low = 0;
+    int size = arr.size();
+    int high = size - 1;
+    int mid;
+
+    while(low<=high){
+        mid = low + (high - low)/2;
+        int missing_count = arr[mid] - (mid+1);
+        if(missing_count < k) low = mid + 1; // Kth missing element is in the right side
+        else high = mid - 1; // Else it is in the left side 
+    }
+    // After the loop the arr[low] will have atleast k missing elements before it
+    // kth missing element = arr[low - 1] + no of missing elements after it
+    // = arr[low - 1] + (k - (arr[low-1] - (low-1+1)))
+    // = low + k
+    return low + k;
+}
+
 int main() {
-    vector<int> arr = {1,2,3,4};
-    cout<<findKthPositive(arr, 2)<<endl;
+    vector<int> arr = {1,2,3,6};
+    cout<<findKthPositive1(arr, 2)<<endl;
+    cout<<findKthPositive2(arr, 2)<<endl;
     return 0;
 }
 
 // OUTPUT:
-// 6
+// 5
+// 5
+
+// Example to explain 2nd Approach:
+// k = 3
+// {1,2,3,4,8,10}
+// l    m     h
+// missing_count = 0 < k
+// move to the right
+
+// {1,2,3,4,8,10}
+//        l m  h
+// missing_count=3 = k
+// move to the left
+
+// {1,2,3,4, 8,10}
+//        lmh
+// missing_count = 0 < k
+// move to the right
+
+// {1,2,3,4, 8,10}
+//        h lm
+
+// answer = low + k = 4 + 3 = 7 
+
+// One more example:
+// k = 2
+// {1,2,3,4}
+//  l m   h
+// missing_count = 0 < k
+// move to the right
+
+// {1,2,3, 4}
+//      lm h
+// missing_count = 0 < k
+// move to the right
+
+// {1,2,3,4  }
+//        lmh
+// missing_count = 0 < k
+// move to the right
+
+// l = 4 and the loop ends
+// Thus answer= low + k = 4 + 2 = 6
+
+// One more example
+// k = 2
+// {1,2,3,6}
+//  l m   h
+// missing_count = 0 < k
+// move to the right
+
+// {1,2,3, 6}
+//      lm h
+// missing_count = 0 < k
+// move to the right
+
+// {1,2,3,6}
+//       lmh
+
+// missing_count = 2 = k
+// move to the left
+
+
+// {1,2,3,6}
+//      h lm
+
+// answer = low + k = 3 + 2 = 5
+
+
