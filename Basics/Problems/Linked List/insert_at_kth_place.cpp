ed48c762f529ed96f20037266792e26df4736a1a
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2024-10-30 14:14:41 +0530

    Add files via upload

---
+++ Basics/Problems/Linked List/insert_at_kth_place.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+struct Node
+{
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1, Node* next1)
+    {
+        data = data1;
+        next = next1;
+    }
+
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node* createLinkedList(vector<int>& arr)
+{
+    Node* head = nullptr;
+    for (int i = arr.size() - 1; i >= 0; --i) {
+        head = new Node(arr[i], head);
+    }
+    return head;
+}
+
+void printLinkedList(Node* head) {
+    Node* temp = head;
+    while(temp)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void insertAtKthPlace(Node*& head, int val, int k)
+{
+    if(k==1)
+    {
+        Node* newHead = new Node(k, head);
+        head = newHead;
+        return;
+    }
+    int count = 0;
+    Node* temp = head;
+    Node* current = temp;
+    while(temp)
+    {
+        count++;
+        if(count == k)
+        {
+            current->next = new Node(val, temp);
+            return;
+        }
+        current = temp;
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    vector<int> arr = {1,5,3,4};
+    Node* head = createLinkedList(arr);
+    insertAtKthPlace(head, 10, 3);
+    printLinkedList(head);
+}
+
+// OUTPUT:
+// 1 5 10 3 4
+
+
