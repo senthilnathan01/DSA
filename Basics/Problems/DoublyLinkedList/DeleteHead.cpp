dff7d706104520b3b49e054471160ea71f674f69
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2024-11-03 15:57:04 +0530

    Add files via upload

---
+++ Basics/Problems/DoublyLinkedList/DeleteHead.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+
+using namespace std;
+
+struct Node
+{
+    public:
+    int data;
+    Node* next;
+    Node* back;
+
+    public:
+    Node(int data1, Node* next1, Node* back1)
+    {
+        data = data1;
+        next = next1;
+        back = back1;
+    }
+
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+        back = nullptr;
+    }
+};
+
+Node* createDLL(vector<int>& arr)
+{
+    Node* head = new Node(arr[0]);
+    Node* prev = head;
+    for(int i = 1; i<arr.size(); i++)
+    {
+        Node* temp = new Node(arr[i], NULL, prev);
+        prev->next = temp;
+        prev = temp;
+    }
+    return head;
+}
+
+void printLinkedList(Node* head) {
+    Node* temp = head;
+    while(temp)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+Node* DeleteHead(Node* head)
+{   
+    if(head == NULL || head->next==NULL) return NULL;
+
+    Node* temp = head;
+    head = head->next;
+
+    head->back = NULL;
+    temp->next = NULL;
+
+    delete temp;
+    return head;
+}
+
+int main()
+{
+    vector<int> arr= {1,3, 5, 10};
+    Node* head = createDLL(arr);
+    printLinkedList(head);
+    head = DeleteHead(head);
+    printLinkedList(head);
+}
+
+// OUTPUT:
+// 1 3 5 10 
+// 3 5 10 
