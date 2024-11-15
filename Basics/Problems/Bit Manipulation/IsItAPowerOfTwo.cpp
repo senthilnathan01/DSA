791b0b7618230561b40149615b6800f786e3b80c
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2024-11-15 11:33:19 +0530

    Add files via upload

---
+++ Basics/Problems/Bit Manipulation/IsItAPowerOfTwo.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n = 16;
+    if(((n)&(n-1))==0) cout<<"Yes";
+    else cout<<"No";
+}
+
+// OUTPUT:
+// Yes
+
+// Explanation
+// 16 -> 1000 
+// 15 -> 0111
+
+// 16 & 15 = 0000 = 0
+// Power of 2
+
+// ----
+
+// 15 -> 0111
+// 14 -> 0110
+
+// 15 & 14 = 0110 != 0 
+// Not a power of 2
+
