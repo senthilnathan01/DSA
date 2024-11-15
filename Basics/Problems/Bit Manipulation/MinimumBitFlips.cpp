a36afcc950c46197cbba1f4053c810417a485486
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2024-11-15 17:08:53 +0530

    Add files via upload

--- Basics/Problems/Bit Manipulation/MinimumBitFlips.cpp
+++ Basics/Problems/Bit Manipulation/MinimumBitFlips.cpp
@@ -1,36 +1,51 @@
 #include<iostream>
+#include<vector>
+#include<unordered_map>
 
 using namespace std;
 /*
 PROBLEM:
-A bit flip of a number x is choosing a bit in the binary representation of x and flipping it from either 0 to 1 or 1 to 0.
+Given a non-empty array of integers nums, every element appears twice except for one. 
+Find that single one.
 
-For example, for x = 7, the binary representation is 111 and we may choose any bit (including any leading zeros not shown) and flip it. 
-We can flip the first bit from the right to get 110, flip the second bit from the right to get 101, flip the fifth bit from the right (a leading zero) to get 10111, etc.
-Given two integers start and goal, return the minimum number of bit flips to convert start to goal.
+You must implement a solution with a linear runtime complexity and use only constant extra space.
 */
 
-int minimumBitFips(int start, int goal)
+// Approach 1: Using Map
+// TC = O(N*logM + M) where M = N/2 + 1
+// SC = O(M)
+
+int singleNumber1(vector<int> nums)
 {
-    int num = start^goal;
-    int count=0;
-    while(num)
-    {
-        num = (num&(num-1));
-        count++;
+    unordered_map<int, int> counts;
+    for(auto num: nums) counts[num]++;
+    for(auto it: counts){
+        if(it.second==1) return it.first;
     }
-    return count;
+    return -1;
+}
+
+// Optimal Approach:
+// TC = O(N)
+// SC = O(1)
+
+int singleNumber2(vector<int> nums)
+{
+    int ans = 0;
+    for(int num: nums) ans = (ans^num);
+    return ans;   
 }
 
 int main()
 {
-    int start = 7;
-    int goal = 10;
-    cout<<minimumBitFips(start, goal);
+    vector<int> array = {4,1,2,1,2};
+    cout<<singleNumber1(array)<<endl;
+    cout<<singleNumber2(array);
 }
 
 // OUTPUT:
-// 3
+// 4
+// 4
 
 
 
