a0b465b66b6e8b04502eae0277b39917882a03bd
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2024-12-11 21:48:08 +0530

    Add files via upload

---
+++ Basics/Problems/Stack/StackImplementationUsingArray.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+
+using namespace std;
+
+// Stack behind the scenes LOL
+class Stack
+{
+    int size;
+    int *arr;
+    int top;
+    public:
+    Stack()
+    {
+        top = -1;
+        size = 1000;
+        arr = new int[size];
+    }
+
+    void Push(int x)
+    {
+        top++;
+        arr[top] = x;
+    }
+
+    int Pop()
+    {
+        int x = arr[top];
+        top--;
+        return x;
+    }
+
+    int Top()
+    {
+        return arr[top];
+    }
+
+    int Size()
+    {
+        return top+1;
+    }
+};
+
+int main()
+{
+    // Let's try out implementation of Stack
+    Stack s;
+    s.Push(6);
+    s.Push(3);
+    cout<<s.Size()<<endl;
+    cout<<s.Top()<<endl;
+    cout<<s.Size()<<endl;
+    cout<<s.Pop()<<endl;
+    cout<<s.Size()<<endl;
+    cout<<s.Top()<<endl;
+
+    return 0;   
+}
+
+// OUTPUT:
+// 2
+// 3
+// 2
+// 3
+// 1
+// 6
