49d73f1d75b90fa11871ea0cb0be899574e2c57f
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2024-12-27 16:01:05 +0530

    Add files via upload

---
+++ Basics/Problems/Binary_Search_Trees/searchBST.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<map>
+#include<set>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+/*
+You are given the root of a binary search tree (BST) and an integer val.
+
+Find the node in the BST that the node's value equals val and return the subtree rooted with that node. 
+If such a node does not exist, return null.
+*/
+
+TreeNode* searchBST(TreeNode* root, int val) {
+    while(!root==NULL && root->val != val)
+    {
+        root = val<root->val ? root->left : root->right;
+    }
+    return root;
+}
+
+int main()
+{
+    TreeNode* root = new TreeNode(4);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(7);
+    root->left->left = new TreeNode(1);
+    root->left->right = new TreeNode(3);
+
+    TreeNode* result = searchBST(root, 2);
+    cout<<result->val<<endl;
+
+    return 0;
+}
+
+// OUTPUT:
+// 2
+
+
+
