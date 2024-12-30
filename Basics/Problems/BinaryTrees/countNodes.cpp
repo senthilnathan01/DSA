44195e8bee3491e029935acd57d8353e91e23f84
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2024-12-30 14:19:59 +0530

    Add files via upload

---
+++ Basics/Problems/BinaryTrees/countNodes.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    void countMe(TreeNode* node, int& cnt)
+    {
+        cnt++;
+        if(node->left != nullptr) countMe(node->left, cnt);
+        if(node->right != nullptr) countMe(node->right, cnt);
+    }
+    int countNodes(TreeNode* root) {
+        if(root==nullptr) return 0;
+        
+        int cnt = 0;
+        countMe(root, cnt);
+        return cnt;
+
+    }
+};
+
+int main()
+{
+    TreeNode* root = new TreeNode(8);
+    root->left = new TreeNode(4);
+    root->right = new TreeNode(12);
+    root->left->left = new TreeNode(2);
+    root->left->right = new TreeNode(6);
+    root->right->left = new TreeNode(10);
+    root->right->right = new TreeNode(14);
+
+    Solution obj;
+    cout<< obj.countNodes(root);
+
+    return 0;
+}
+
+// OUTPUT:
+// 7
