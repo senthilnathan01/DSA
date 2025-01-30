2a8d5c0615ce60422df0ed9dd65fa92e487ad645
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2025-01-31 00:57:42 +0530

    Add files via upload

---
+++ Basics/Problems/Graphs/problems_on_BFS_DFS/CycleUndirectedGraph_usingDFS.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+
+using namespace std;
+
+/*
+Given an undirected graph with V vertices labelled from 0 to V-1 and E edges, 
+check whether the graph contains any cycle or not. The Graph is represented as 
+an adjacency list, where adj[i] contains all the vertices that are directly connected to vertex i.
+
+NOTE: The adjacency list represents undirected edges, meaning that if there is 
+an edge between vertex i and vertex j, both j will be adj[i] and i will be in adj[j].
+*/
+
+// Using DFS
+
+class Solution {
+    private:
+    bool dfs(int node, int parent, bool visited[], vector<vector<int>> &adj)
+    {
+        visited[node] = true;
+        for(auto adjNode: adj[node])
+        {
+            if(!visited[adjNode])
+            {
+                if(dfs(adjNode, node, visited, adj)) return true;
+            }
+            else if(adjNode != parent) return true;
+        }
+        return false;
+    }
+
+    public:
+    bool isCycle(vector<vector<int>> &adj)
+    {
+        int V = adj.size();
+        bool visited[V] ={false};
+        return dfs(1, -1, visited, adj);
+    }
+
+};
+
+int main()
+{
+    vector<vector<int>> adj  = {{1}, 
+                                {0,2,4},
+                                {1,3},
+                                {2,4},
+                                {1,3}};
+    
+    Solution obj;
+    cout<<obj.isCycle(adj);
+
+    return 0;
+}
+
+// OUTPUT:
+// 1
