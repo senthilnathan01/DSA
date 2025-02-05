9cf8dbce50b9e54c7259a9312ee79e30ffad5bb8
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2025-02-06 00:09:29 +0530

    Update wordLadderII.cpp

--- Basics/Problems/Graphs/problems_on_BFS_DFS/wordLadderII.cpp
+++ Basics/Problems/Graphs/problems_on_BFS_DFS/wordLadderII.cpp
@@ -106,7 +106,93 @@ public:
 };
 
 // Another way to write:
-class Solution2
+class Solution2 {
+public:
+vector<vector<string>> findLadders(string beginWord, string endWord, vector<string> &wordList)
 {
+    // Approach:
+    /*
+    BFS for Shortest Path:
 
+    - Use BFS to explore all possible transformations from the beginWord.
+    - Maintain a depthMap to record the minimum number of transformations required to reach each word.
+    - If the endWord is reached during BFS, the search stops, and the depth of the endWord is noted.
+    
+    DFS for Path Construction:
+
+    - Once the BFS is complete, use DFS to backtrack from the endWord to the beginWord, constructing 
+    all valid paths that match the minimum transformation depth recorded in depthMap.
+    - Each valid path is stored in the result list.
+    */
+    unordered_map<string, int> depthMap;
+    vector<vector<string>> ans;
+
+    // BFS to find the shortest path
+    unordered_set<string> wordSet(wordList.begin(), wordList.end());
+    queue<string> q;
+    q.push(beginWord);
+    depthMap[beginWord] = 1;
+    wordSet.erase(beginWord);
+
+    while(!q.empty())
+    {
+        string word = q.front();
+        q.pop();
+        int steps = depthMap[word];
+        if (word == endWord) break;
+        for(int i = 0; i < word.size(); i++)
+        {
+            char original = word[i];
+            for(char ch = 'a'; ch <= 'z'; ch++)
+            {
+                word[i] = ch;
+                if(wordSet.count(word))
+                {
+                    q.push(word);
+                    wordSet.erase(word);
+                    depthMap[word] = steps + 1;
+                }
+            }
+            word[i] = original;
+        }
+    }
+
+    // DFS to find all paths
+    if (depthMap.count(endWord))
+    {
+        vector<string> seq = {endWord};
+        dfs(endWord, beginWord, seq, depthMap, ans);
+    }
+
+    return ans;
 }
+
+private:
+void dfs(string word, string beginWord, vector<string> &seq, unordered_map<string, int> &depthMap, vector<vector<string>> &ans)
+{
+    if(word == beginWord)
+    {
+        reverse(seq.begin(), seq.end());
+        ans.push_back(seq);
+        reverse(seq.begin(), seq.end());
+        return;
+    }
+
+    int steps = depthMap[word];
+    for(int i = 0; i < word.size(); i++)
+    {
+        char original = word[i];
+        for(char ch = 'a'; ch <= 'z'; ch++)
+        {
+            word[i] = ch;
+            if(depthMap.count(word) && depthMap[word] + 1 == steps)
+            {
+                seq.push_back(word);
+                dfs(word, beginWord, seq, depthMap, ans);
+                seq.pop_back();
+            }
+        }
+        word[i] = original;
+    }
+}
+};
