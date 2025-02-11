4021e3956b285e08fd3dacacfe4e8362740eb6aa
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2025-02-12 01:56:38 +0530

    Create dijkstra.cpp

---
+++ Basics/Problems/Graphs/shortestPath/dijkstra.cpp
@@ -0,0 +1,8 @@
+/*
+Given a weighted, undirected and connected graph where you have given adjacency list adj. 
+You have to find the shortest distance of all the vertices from the source vertex src, and
+return a list of integers denoting the shortest distance between each node and source vertex src.
+
+Note: The Graph doesn't contain any negative weight edge.
+*/
+
