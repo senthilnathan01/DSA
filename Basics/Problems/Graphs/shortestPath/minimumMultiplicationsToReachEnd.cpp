9438c1805db885273059cbd6fff8b962067f62cd
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2025-02-25 01:06:54 +0530

    Create minimumMultiplicationsToReachEnd.cpp

---
+++ Basics/Problems/Graphs/shortestPath/minimumMultiplicationsToReachEnd.cpp
@@ -0,0 +1,8 @@
+/*
+Given start, end and an array arr of n numbers. At each step, start is multiplied with any 
+number in the array and then mod operation with 100000 is done to get the new start.
+
+Your task is to find the minimum steps in which end can be achieved starting from start. 
+If it is not possible to reach end, then return -1.
+*/
+
