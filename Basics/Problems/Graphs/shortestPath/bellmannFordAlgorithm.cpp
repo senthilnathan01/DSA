005ccd104e457d44d4731f5cbab09ff9207a5e48
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2025-02-26 01:11:32 +0530

    Create bellmannFordAlgorithm.cpp

---
+++ Basics/Problems/Graphs/shortestPath/bellmannFordAlgorithm.cpp
@@ -0,0 +1,8 @@
+/*
+Given a weighted and directed graph of v vertices and edges, Find the shortest 
+distance of all the vertex's from the source vertex, src and return a list of 
+integers where the ith integer denotes the distance of the ith node from the 
+source node. If a vertices can't be reach from the s then mark the distance as 10^8.
+Note: If there exist a path to a negative weighted cycle from the source node then return {-1}.
+  */
+
