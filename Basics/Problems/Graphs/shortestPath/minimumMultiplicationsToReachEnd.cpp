06104547f3cdef619612bfde5bcb8a16fed6caba
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2025-02-25 23:43:13 +0530

    Update minimumMultiplicationsToReachEnd.cpp

--- Basics/Problems/Graphs/shortestPath/minimumMultiplicationsToReachEnd.cpp
+++ Basics/Problems/Graphs/shortestPath/minimumMultiplicationsToReachEnd.cpp
@@ -6,3 +6,35 @@ Your task is to find the minimum steps in which end can be achieved starting fro
 If it is not possible to reach end, then return -1.
 */
 
+class Solution {
+    typedef pair<int, int> P;
+  public:
+    int minimumMultiplications(vector<int>& arr, int start, int end) {
+        // code here
+        // Base Case
+        if(start == end) return 0;
+        int mod = 100000;
+        queue<P> q;
+        q.push({start, 0});
+        vector<int> dist(100000, 1e9);
+        dist[start] = 0;
+        while(!q.empty())
+        {
+            int node = q.front().first;
+            int steps = q.front().second;
+            q.pop();
+            
+            for(auto it: arr)
+            {
+                int num = (it*node) % mod;
+                if(steps + 1 < dist[num])
+                {
+                    dist[num] = steps + 1;
+                    if(num == end) return steps + 1;
+                    q.push({num, steps + 1});
+                }
+            }
+        }
+        return -1;
+    }
+};
