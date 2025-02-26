54d7ff8b7edd84a2eec25491dd0661868383f3a2
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2025-02-26 23:13:57 +0530

    Update bellmannFordAlgorithm.cpp

--- Basics/Problems/Graphs/shortestPath/bellmannFordAlgorithm.cpp
+++ Basics/Problems/Graphs/shortestPath/bellmannFordAlgorithm.cpp
@@ -6,3 +6,41 @@ source node. If a vertices can't be reach from the s then mark the distance as 1
 Note: If there exist a path to a negative weighted cycle from the source node then return {-1}.
   */
 
+class Solution {
+  public:
+    /*  Function to implement Bellman Ford
+     *   edges: vector of vectors which represents the graph
+     *   src: source vertex
+     *   V: number of vertices
+     */
+    vector<int> bellmanFord(int V, vector<vector<int>>& edges, int src) {
+        // Code here
+        vector<int> dist(V, 1e8);
+        dist[src] = 0;
+        for (int i = 0; i < V; i++)
+        {
+            for (auto it: edges)
+            {
+                int u = it[0];
+                int v = it[1];
+                int wt = it[2];
+                if(dist[u] != 1e8 && dist[u] + wt < dist[v])
+                {
+                    dist[v] = dist[u] + wt;
+                }
+            }
+        }
+        // Nth relaxation to check negative cycle
+        for(auto it: edges)
+        {
+            int u = it[0];
+            int v = it[1];
+            int wt = it[2];
+            if(dist[u] != 1e8 && dist[u] + wt < dist[v])
+            {
+                return {-1};
+            }
+        }
+        return dist;
+    }
+};
