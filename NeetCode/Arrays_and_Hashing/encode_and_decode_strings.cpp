7a7c3a8cb3470c2c94486ba379aad29482a63327
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2025-07-24 15:56:43 +0530

    Create encode_and_decode_strings.cpp

---
+++ NeetCode/Arrays_and_Hashing/encode_and_decode_strings.cpp
@@ -0,0 +1,24 @@
+/*
+Given an array of strings s[], you are required to create an algorithm in the encode() function that can convert the given strings into a single encoded string, which can be transmitted over the network and then decoded back into the original array of strings. The decoding will happen in the decode() function.
+
+You need to implement two functions:
+1. encode(): This takes an array of strings s[] and encodes it into a single string. 
+2. decode(): This takes the encoded string as input and returns an array of strings containing the original array as given in the encode method.
+
+Note: You are not allowed to use any inbuilt serialize method.  
+
+Examples:
+
+Input: s = ["Hello","World"]
+Output: ["Hello","World"]
+
+GFG Link: https://www.geeksforgeeks.org/problems/encode-and-decode-strings/1
+*/
+
+// Solution 1:
+
+
+
+
+
+
