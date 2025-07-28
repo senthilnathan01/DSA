79672a6952b7d5f8f4df31a9fdef0fa373335c33
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2025-07-28 18:36:53 +0530

    Added second approach (used stacks)

--- NeetCode/stacks/largest_rectangle_in_histogram.cpp
+++ NeetCode/stacks/largest_rectangle_in_histogram.cpp
@@ -45,3 +45,64 @@ public:
 // We will be using Stacks for this
 // And solution 3 and 4 also uses Stack but optimises the process further
 // Watch NeetCode video solution for detailed explanation. Its good!
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+
+        // Initialise the leftB and rightB
+        vector<int> leftB(n, -1);
+        vector<int> rightB(n, n);
+
+        // We use stack to store the indices
+        stack<int> st;
+
+        // Get the left Boundaries
+        for(int i = 0; i < n; i++)
+        {
+            while(!st.empty() && heights[st.top()] >= heights[i]) st.pop();
+
+            if(!st.empty()) leftB[i] = st.top();
+
+            st.push(i);
+        }
+
+        // For ex:
+        // Heights= [ 2,  1, 5, 6, 2, 3]
+        // leftB  = [-1, -1, 1, 2, 1, 4]
+
+        // Clear the stack
+        st = {};
+
+        // Get the right boundaries
+        // Just do the same thing as before but iterate from right to left
+        for(int i = n- 1; i >= 0; i--)
+        {
+            while(!st.empty() && heights[st.top()] >= heights[i]) st.pop();
+
+            if(!st.empty()) rightB[i] = st.top();
+
+            st.push(i);
+        }
+
+        // For ex:
+        // Heights= [ 2,  1, 5, 6, 2, 3]
+        // leftB  = [-1, -1, 1, 2, 1, 4] -> [0, 0, 2, 3, 2, 5]
+        // rightB = [ 1,  6, 3, 4, 6, 6] -> [0, 5, 2, 3, 5, 5]
+
+        // Get the max Area
+        int maxArea = 0;
+        for(int i = 0; i < n; i++)
+        {
+            leftB[i]++;
+            rightB[i]--;
+            maxArea = max(maxArea, heights[i]*(rightB[i]-leftB[i]+1));
+            // We add 1 cuz we include the area of the box i itself 
+        }   
+
+        return maxArea;
+    }
+};
+
+// Solution 3:
+// Let's use stack again but with one pass
