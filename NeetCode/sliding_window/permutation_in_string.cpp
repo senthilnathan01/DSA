f63c02c97bfcc046d0efdf955e8cfe0d96a8339b
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2025-07-30 19:15:05 +0530

    problem statement

---
+++ NeetCode/sliding_window/permutation_in_string.cpp
@@ -0,0 +1,10 @@
+/*
+Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+
+In other words, return true if one of s1's permutations is the substring of s2.
+
+A permutation is a rearrangement of all the characters of a string.
+
+Leetcode Link: https://leetcode.com/problems/permutation-in-string/description/
+*/
+
