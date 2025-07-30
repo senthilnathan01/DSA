7e4245e8cf63f475897fa97fbebd59c2aaf228c8
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2025-07-30 13:42:12 +0530

    Create time_based_key_value_store.cpp

---
+++ NeetCode/binary_search/time_based_key_value_store.cpp
@@ -0,0 +1,37 @@
+/*
+Design a time-based key-value data structure that can store multiple values for the same key at different time stamps and retrieve the key's value at a certain timestamp.
+
+Implement the TimeMap class:
+
+TimeMap() Initializes the object of the data structure.
+void set(String key, String value, int timestamp) Stores the key key with the value value at the given time timestamp.
+String get(String key, int timestamp) Returns a value such that set was called previously, with timestamp_prev <= timestamp. 
+If there are multiple such values, it returns the value associated with the largest timestamp_prev. If there are no values, it returns "".
+
+Leetcode Link: https://leetcode.com/problems/time-based-key-value-store/description/
+*/
+
+class TimeMap {
+public:
+    unordered_map<string, map<int, string>> m;
+
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        m[key].insert({timestamp, value});
+    }
+    
+    string get(string key, int timestamp) {
+        auto it = m[key].upper_bound(timestamp);
+        return it == m[key].begin() ? "" : prev(it)->second;
+    }
+};
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap* obj = new TimeMap();
+ * obj->set(key,value,timestamp);
+ * string param_2 = obj->get(key,timestamp);
+ */
