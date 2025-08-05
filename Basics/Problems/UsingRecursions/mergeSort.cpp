2a880be33d64519b3acb2847bc4bad10327abc1e
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2025-08-05 15:24:45 +0530

    Create mergeSort.cpp

---
+++ Basics/Problems/UsingRecursions/mergeSort.cpp
@@ -0,0 +1,50 @@
+class Solution {
+  public:
+    void merge(vector<int> &arr, int low, int mid, int high)
+    {
+        vector<int> temp;
+        int l = low, r = mid + 1;
+        while(l <= mid && r <= high)
+        {
+            if(arr[l]<=arr[r])
+            {
+                temp.push_back(arr[l]);
+                l++;
+            }
+            else 
+            {
+                temp.push_back(arr[r]);
+                r++;
+            }
+        }
+    
+        // Still more left in the first arr
+        if(l<=mid)
+        {
+            temp.insert(temp.end(), arr.begin() + l, arr.begin() + mid + 1);
+        }
+        
+        // Or still more left in the right arr
+        if(r <= high)
+        {
+            temp.insert(temp.end(), arr.begin() + r, arr.begin() + high + 1);
+        }
+        
+        for(int i = low; i <= high; i++)
+        {
+            arr[i] = temp[i - low];    
+        }
+    }
+
+    void mergeSort(vector<int>& arr, int l, int r) {
+        // Base Case
+        int low = l, high = r;
+        if(low == high) return;
+    
+        // Recursive Case
+        int mid = (low + high)/2;
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid+1, high);
+        merge(arr, low, mid, high);
+    }
+};
