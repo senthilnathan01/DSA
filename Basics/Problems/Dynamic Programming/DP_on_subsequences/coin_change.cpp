321869f3f2fa5470dc43d5ffcfb91f7bddf869a1
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2025-08-27 16:19:19 +0530

    Create coin_change.cpp

---
+++ Basics/Problems/Dynamic Programming/DP_on_subsequences/coin_change.cpp
@@ -0,0 +1,27 @@
+/*
+You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+You may assume that you have an infinite number of each kind of coin.
+
+Link: https://leetcode.com/problems/coin-change/description/
+*/
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
+
+        for(int coin: coins){
+            for(int j = 0; j <=amount - coin; j++){
+                if(dp[j]!=INT_MAX){
+                    dp[j+coin] = min(dp[j+coin], 1 + dp[j]);
+                }
+            }
+        }
+
+        return dp[amount] == INT_MAX ? -1: dp[amount];
+    }
+};
