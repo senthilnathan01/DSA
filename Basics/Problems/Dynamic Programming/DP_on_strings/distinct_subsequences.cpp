914dc4cd7e62dbc96e016aab57803913d73ac322
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2025-08-30 16:09:07 +0530

    Update distinct_subsequences.cpp

--- Basics/Problems/Dynamic Programming/DP_on_strings/distinct_subsequences.cpp
+++ Basics/Problems/Dynamic Programming/DP_on_strings/distinct_subsequences.cpp
@@ -25,3 +25,25 @@ public:
         return (int)dp[n][m];
     }
 };
+
+// Optimized further
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n = s.size(), m = t.size();
+
+        vector<uint64_t> prev(m+1, 0), curr(m+1, 0);
+        prev[0] = 1;
+
+        for(int i = 1; i <= n; i++){
+            curr[0] = 1;
+            for(int j = 1; j <= m; j++){
+                if(s[i-1]==t[j-1]) curr[j] = prev[j-1] + prev[j];
+                else curr[j] = prev[j];
+            }
+            prev.swap(curr);
+        }
+
+        return (int)prev[m];
+    }
+};
