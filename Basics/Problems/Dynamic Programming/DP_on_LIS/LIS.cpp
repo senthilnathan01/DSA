f874dbf69c14c6464c2804bd78f8140529d04f1a
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2025-09-10 00:33:39 +0530

    Update LIS.cpp

--- Basics/Problems/Dynamic Programming/DP_on_LIS/LIS.cpp
+++ Basics/Problems/Dynamic Programming/DP_on_LIS/LIS.cpp
@@ -78,3 +78,20 @@ public:
         return maxi;
     }
 };
+
+// So far all the approaches where O(N^2) 
+// Another brilliant approach with TC = O(NlogN)
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> sub;
+        for(int x : nums) {
+            auto it = lower_bound(sub.begin(), sub.end(), x);
+            if(it == sub.end())
+                sub.push_back(x);
+            else
+                *it = x;
+        }
+        return sub.size();
+    }
+};
