27f06e064f7aca60d9a174c2220a50829a2015fd
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2025-09-11 18:37:29 +0530

    Create coin_change_2.cpp

---
+++ NeetCode/2D_DP/coin_change_2.cpp
@@ -0,0 +1,22 @@
+/*
+You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+Return the number of combinations that make up that amount. If that amount of money cannot be made up by any combination of the coins, return 0.
+
+You may assume that you have an infinite number of each kind of coin.
+
+The answer is guaranteed to fit into a signed 32-bit integer.
+
+LINK: https://leetcode.com/problems/coin-change-ii/description/
+*/
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<unsigned int> dp(amount+1, 0);
+        dp[0] = 1;
+        for(int coin: coins){
+            for(int j = coin; j <= amount; j++) dp[j] += dp[j-coin];
+        }
+        return (int)dp[amount];
+    }
+};
