7b5dbee3fb1d576f10a65563113080524a127ce4
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2025-09-13 01:41:14 +0530

    Create burst_balloons.cpp

---
+++ NeetCode/2D_DP/burst_balloons.cpp
@@ -0,0 +1,38 @@
+/*
+You are given n balloons, indexed from 0 to n - 1. 
+  Each balloon is painted with a number on it represented by an array nums. You are asked to burst all the balloons.
+
+If you burst the ith balloon, you will get nums[i - 1] * nums[i] * nums[i + 1] coins. 
+  If i - 1 or i + 1 goes out of bounds of the array, then treat it as if there is a balloon with a 1 painted on it.
+
+Return the maximum coins you can collect by bursting the balloons wisely.
+
+LINK: https://leetcode.com/problems/burst-balloons/description/
+*/
+
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int func(int i, int j, vector<int> &arr){
+        if(i>j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        int maxi = 0;
+        for(int k = i; k <= j; k++){
+            maxi = max(maxi, arr[i-1]*arr[k]*arr[j+1] + func(i, k-1, arr) + func(k+1, j, arr));
+        }
+        return dp[i][j] = maxi;
+    }
+    int maxCoins(vector<int>& nums) {
+        vector<int> arr = {1};
+        arr.insert(arr.end(), nums.begin(), nums.end());
+        arr.push_back(1);   
+        int n = nums.size();
+
+        dp.assign(n+2, vector<int>(n+2, -1));
+
+        return func(1, n, arr);
+    }
+};
+
+// Tabulation Approach
