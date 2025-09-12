f2400cce502c78bb97213a7eb0a25ea24526a2b3
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2025-09-12 17:29:11 +0530

    Update interleaving_string.cpp

--- NeetCode/2D_DP/interleaving_string.cpp
+++ NeetCode/2D_DP/interleaving_string.cpp
@@ -22,16 +22,15 @@ public:
 
         vector<vector<bool>> dp(a+1, vector<bool>(b+1, 0));
         dp[0][0] = true;
-        for(int i = 1; i <= a; i++) if(dp[i-1][0] && s1[i-1]==s3[i-1]) dp[i][0] = true;
-        for(int j = 1; j <= b; j++) if(dp[0][j-1] && s2[j-1]==s3[j-1]) dp[0][j] = true;
+        for(int i = 1; i <= a; i++) dp[i][0] = (dp[i-1][0] && s1[i-1]==s3[i-1]);
+        for(int j = 1; j <= b; j++) dp[0][j] = (dp[0][j-1] && s2[j-1]==s3[j-1]);
 
         for(int i = 1; i <= a; i++){
             for(int j = 1; j <= b; j++){
-                if((dp[i-1][j] && s1[i-1] == s3[i+j-1]) ||
-                (dp[i][j-1] && s2[j-1]==s3[i+j-1])) dp[i][j] = true;
+                dp[i][j] = ((dp[i-1][j] && s1[i-1] == s3[i+j-1]) ||
+                (dp[i][j-1] && s2[j-1]==s3[i+j-1]));
             }
         }
-
         return dp[a][b];
     }
 };
