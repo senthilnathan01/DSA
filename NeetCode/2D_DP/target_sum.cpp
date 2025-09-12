def980426d6bb2357f8fba86d75f92bf636fa439
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2025-09-12 15:00:36 +0530

    Update target_sum.cpp

--- NeetCode/2D_DP/target_sum.cpp
+++ NeetCode/2D_DP/target_sum.cpp
@@ -9,3 +9,41 @@ Return the number of different expressions that you can build, which evaluates t
 LINK: https://leetcode.com/problems/target-sum/description/
 */
 
+class Solution {
+public:
+    unordered_map<string, int> dp;
+    int func(int idx, int n, vector<int> &nums, int target){
+        if(idx==n) return target==0 ? 1 : 0;
+        string key = to_string(idx) + ',' + to_string(target);
+        if(dp.find(key)!=dp.end()) return dp[key];
+        return dp[key] = func(idx+1, n, nums, target+nums[idx]) + func(idx+1, n, nums, target-nums[idx]);
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        return func(0, nums.size(), nums, target);
+    }
+};
+
+// Better memoization instead of string concatenation
+class Solution {
+public:
+    int dp[21][2001];
+    vector<int> suff;
+
+    int func(int idx, int n, vector<int> &nums, int target){
+        if(idx==n) return target==0 ? 1 : 0;
+
+        if (abs(target) > suff[idx]) return 0;
+        
+        int offset = target + 1000;
+        if(dp[idx][offset]!=-1) return dp[idx][offset];
+
+        return dp[idx][offset] = func(idx+1, n, nums, target+nums[idx]) + func(idx+1, n, nums, target-nums[idx]);
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        memset(dp, -1, sizeof(dp));
+        int n = nums.size();
+        suff.assign(n+1, 0);
+        for(int i = n-1; i >= 0; i--) suff[i] = suff[i+1] + nums[i];
+        return func(0, n, nums, target);
+    }
+};
