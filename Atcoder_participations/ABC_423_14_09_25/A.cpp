f572e61f6a996f70d4a280b2b968783cd81d0d32
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2025-09-14 19:12:03 +0530

    Create A.cpp

---
+++ Atcoder_participations/ABC_423_14_09_25/A.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+
+int main()
+{
+    int x, c; cin>>x>>c;
+    cout<<(x/(1000+c))*1000;
+    return 0;
+}
