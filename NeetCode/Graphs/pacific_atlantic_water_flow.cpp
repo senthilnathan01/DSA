03f28318c0f1dba15a28933d739e2dc89708aea9
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2025-09-14 22:53:28 +0530

    Update pacific_atlantic_water_flow.cpp

--- NeetCode/Graphs/pacific_atlantic_water_flow.cpp
+++ NeetCode/Graphs/pacific_atlantic_water_flow.cpp
@@ -13,3 +13,37 @@ Return a 2D list of grid coordinates result where result[i] = [ri, ci] denotes t
 LINK: https://leetcode.com/problems/pacific-atlantic-water-flow/description/
 */
 
+class Solution {
+public:
+    vector<pair<int, int>> dirs = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+    void dfs(int r, int c, int m, int n, vector<vector<bool>> &vis, vector<vector<int>> &heights){
+        vis[r][c] = true;
+        for(auto [dr, dc]: dirs){
+            int nr = r + dr, nc = c + dc;
+            if(nr < 0 || nr >=m || nc <0 || nc>=n) continue;
+            if(vis[nr][nc] || heights[nr][nc]<heights[r][c]) continue;
+            dfs(nr, nc, m, n, vis, heights);
+        }
+    }
+
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        int m = heights.size(), n = heights[0].size();
+        vector<vector<bool>> p(m, vector<bool>(n, false));
+        vector<vector<bool>> a(m, vector<bool>(n, false));
+        vector<vector<int>> res;
+        for(int i = 0; i < m; i++){
+            dfs(i, 0, m, n, p, heights);
+            dfs(i, n-1, m, n, a, heights);
+        }
+
+        for(int j = 0; j < n; j++){
+            dfs(0, j, m, n, p, heights);
+            dfs(m-1, j, m, n, a, heights);
+        }
+
+        for(int i= 0; i < m; i++){
+            for(int j = 0; j < n; j++) if(p[i][j] && a[i][j]) res.push_back({i, j});
+        }
+        return res;
+    }
+};
