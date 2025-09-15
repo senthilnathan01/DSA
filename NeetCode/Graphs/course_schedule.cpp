992d9911cef7ba88f366c95cf81c71c7f2ba8a8f
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2025-09-15 11:33:42 +0530

    Update course_schedule.cpp

--- NeetCode/Graphs/course_schedule.cpp
+++ NeetCode/Graphs/course_schedule.cpp
@@ -8,3 +8,36 @@ Return true if you can finish all courses. Otherwise, return false.
 LINK: https://leetcode.com/problems/course-schedule/description/
 */
 
+// If there is a cycle we won't be able to visit all the nodes
+// Used Kahn's algo for topo sort here
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses, 0);
+        for(auto it: prerequisites){
+            int u = it[1]; // check the order
+            int v = it[0];
+            adj[u].push_back(v);
+            indegree[v]++;
+        }
+
+        queue<int> q;
+        for(int i = 0; i < numCourses; i++) if(indegree[i]==0) q.push(i);
+
+        int cnt = 0;
+
+        while(!q.empty()){
+            int node = q.front();
+            cnt++;
+            q.pop();
+
+            for(auto it: adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
+            }
+        }
+
+        return (cnt == numCourses);
+    }
+};
