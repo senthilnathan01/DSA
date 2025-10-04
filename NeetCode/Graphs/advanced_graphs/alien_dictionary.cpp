eb0f1926bd7797da1717a09c1924e80990b555a2
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2025-10-04 13:09:47 +0530

    Update alien_dictionary.cpp

--- NeetCode/Graphs/advanced_graphs/alien_dictionary.cpp
+++ NeetCode/Graphs/advanced_graphs/alien_dictionary.cpp
@@ -17,3 +17,54 @@ Note: Your implementation will be tested using a driver code.
 LINK: https://www.geeksforgeeks.org/problems/alien-dictionary/1
 */
 
+class Solution {
+  public:
+    string findOrder(vector<string> &words) {
+        vector<vector<int>> adj(26);
+        vector<int> indegree(26, 0);
+        vector<bool> exists(26, false);
+        
+        for(const string &word: words){
+            for(char ch: word) exists[ch-'a'] = true;
+        }
+        
+        for(int i = 0; i+1< words.size(); i++){
+            const string &w1 = words[i];
+            const string &w2 = words[i+1];
+            int len = min(w1.length(), w2.length());
+            int j = 0;
+            
+            while(j<len && w1[j]==w2[j]) j++;
+            
+            if(j < len){
+                int u = w1[j]-'a';
+                int v = w2[j]-'a';
+                adj[u].push_back(v);
+                indegree[v]++;
+            }
+            else if(w1.size()>w2.size()) return "";
+        }
+        
+        queue<int> q;
+        for(int i = 0; i < 26; i++){
+            if(exists[i] && indegree[i]==0) q.push(i);
+        }
+        
+        string res;
+        while(!q.empty()){
+            int u = q.front(); q.pop();
+            res += (char)(u + 'a');
+            
+            for(int v: adj[u]){
+                indegree[v]--;
+                if(indegree[v]==0) q.push(v);
+            }
+        }
+        
+        for(int i = 0; i < 26; i++){
+            if(exists[i] && indegree[i]!=0) return "";
+        }
+        
+        return res;
+    }
+};
