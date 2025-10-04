4777c7d3092348bb6678d7db255b4178a3e3162f
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2025-10-04 22:27:37 +0530

    Create kth_largest_element_in_a_stream.cpp

---
+++ NeetCode/priority_queue/kth_largest_element_in_a_stream.cpp
@@ -0,0 +1,22 @@
+/*
+You are part of a university admissions office and need to keep track of the kth highest test score from applicants in real-time. 
+  This helps to determine cut-off marks for interviews and admissions dynamically as new applicants submit their scores.
+
+You are tasked to implement a class which, for a given integer k, maintains a stream of test scores and continuously returns the 
+kth highest test score after a new score has been submitted. More specifically, we are looking for the kth highest score in the sorted list of all scores.
+
+Implement the KthLargest class:
+
+KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of test scores nums.
+int add(int val) Adds a new test score val to the stream and returns the element representing the kth largest element in the pool of test scores so far.
+
+LINK: https://leetcode.com/problems/kth-largest-element-in-a-stream/description/
+*/
+
+// Another way to do this without min heap is to use an array
+// sort it first
+// because this is sorted, it becomes easier to insert new element in O(logN) time
+// getting the kth largest is O(1) as we are using array 
+// and just need to return array[array.size()-k] (if sorted in ascending)
+
+// But here let's use priority_queue
