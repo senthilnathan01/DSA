1c61b7498ddd8c76732507d64cd25b3f60724301
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2025-10-06 12:58:34 +0530

    Update kth_largest_element_in_a_stream.cpp

--- NeetCode/priority_queue/kth_largest_element_in_a_stream.cpp
+++ NeetCode/priority_queue/kth_largest_element_in_a_stream.cpp
@@ -13,10 +13,67 @@ int add(int val) Adds a new test score val to the stream and returns the element
 LINK: https://leetcode.com/problems/kth-largest-element-in-a-stream/description/
 */
 
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
+
 // Another way to do this without min heap is to use an array
 // sort it first
 // because this is sorted, it becomes easier to insert new element in O(logN) time
 // getting the kth largest is O(1) as we are using array 
 // and just need to return array[array.size()-k] (if sorted in ascending)
+class KthLargest {
+public:
+    vector<int> stream;
+    int k;
 
-// But here let's use priority_queue
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+        for(int num: nums) stream.push_back(num);
+        sort(stream.begin(), stream.end());
+    }
+    
+    int add(int val) {
+        int index = getIndex(val);
+        stream.insert(stream.begin()+index, val);
+        return stream[stream.size()-k];
+    }
+
+private:
+    int getIndex(int val){
+        int l = 0, r = stream.size()-1;
+        // Do BS
+        while(l<=r){
+            int m = (l+r)/2;
+            int midVal = stream[m];
+            if(midVal==val) return m;
+            if(midVal > val) r = m-1;
+            else l = m+1;
+        }
+        // left has moved past all elements smaller than val
+        // current position of left is the one to be returned
+        return l;
+    }
+};
+
+// Using Priority Queue
+class KthLargest {
+public:
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int k;
+
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+        for(int num: nums) add(num);
+    }
+    
+    int add(int val) {
+        if(pq.size()<k||pq.top()<val){
+            pq.push(val);
+            if(pq.size()>k) pq.pop();
+        }
+        return pq.top();
+    }
+};
