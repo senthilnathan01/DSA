f4bb8786a3122bcfa827319ef49fa7fe6d8e8ff1
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2025-10-31 15:32:36 +0530

    Update step_by_step_directions_from_a_binary_tree_node_to_another.cpp

--- CompanywiseQuestions/Google/step_by_step_directions_from_a_binary_tree_node_to_another.cpp
+++ CompanywiseQuestions/Google/step_by_step_directions_from_a_binary_tree_node_to_another.cpp
@@ -12,4 +12,43 @@ Return the step-by-step directions of the shortest path from node s to node t.
 
 LINK: https://leetcode.com/problems/step-by-step-directions-from-a-binary-tree-node-to-another/description/
 */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool getPath(TreeNode* node, int value, string &path){
+        if(node==NULL) return false;
+        if(node->val == value) return true;
 
+        path += 'L';
+        if(getPath(node->left, value, path)) return true;
+        path.pop_back();
+
+        path += 'R';
+        if(getPath(node->right, value, path)) return true;
+        path.pop_back();
+
+        return false;
+    }
+
+    string getDirections(TreeNode* root, int startValue, int destValue) {
+        string pathToStart, pathToDest;
+        if(!getPath(root, startValue, pathToStart)) return "";
+        if(!getPath(root, destValue, pathToDest)) return "";
+
+        int i = 0;
+        while (i < pathToStart.size() && i < pathToDest.size() && pathToStart[i] == pathToDest[i]) i++;
+
+        string up(pathToStart.size()-i, 'U');
+        return up + pathToDest.substr(i); 
+    }
+};
