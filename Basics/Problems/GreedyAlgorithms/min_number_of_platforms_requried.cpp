2c078a0d77eeeb77dc7dfd35bd347aa3210c9ffb
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2025-11-02 01:09:08 +0530

    Create min_number_of_platforms_requried.cpp

---
+++ Basics/Problems/GreedyAlgorithms/min_number_of_platforms_requried.cpp
@@ -0,0 +1,12 @@
+/*
+Given arrival arr[] and departure dep[] times of trains on the same day, find the minimum number of platforms needed so that no train waits. 
+A platform cannot serve two trains at the same time; if a train arrives before another departs, an extra platform is needed.
+
+Note: Time intervals are in the 24-hour format (HHMM) , where the first two characters represent hour (between 00 to 23 ) and 
+the last two characters represent minutes (this will be <= 59 and >= 0). Leading zeros for hours less than 10 are optional (e.g., 0900 is the same as 900).
+
+LINK: https://www.geeksforgeeks.org/problems/minimum-platforms-1587115620/1
+
+#medium
+*/
+
