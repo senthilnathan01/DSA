11c19018441000bd415a757df0c86f029c47da51
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2025-12-22 19:53:18 +0530

    Add problem link for delete middle node
    Added a link to the problem description for reference.

--- Basics/Problems/Linked List/delete_middle_node.cpp
+++ Basics/Problems/Linked List/delete_middle_node.cpp
@@ -4,6 +4,8 @@ You are given the head of a linked list. Delete the middle node, and return the 
 The middle node of a linked list of size n is the ⌊n / 2⌋th node from the start using 0-based indexing, where ⌊x⌋ denotes the largest integer less than or equal to x.
 
 For n = 1, 2, 3, 4, and 5, the middle nodes are 0, 1, 1, 2, and 2, respectively.
+
+LINK: https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/description/
 */
 
 /**
