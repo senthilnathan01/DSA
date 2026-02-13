4855aaa46a0b1f8a34d67ab81442c1c306758270
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2026-02-13 10:52:28 +0530

    Add deep copy function for random pointer linked list
    Implement deep copy for a linked list with random pointers.

---
+++ CompanywiseQuestions/amazon/copy_list_with_random_pointer.cpp
@@ -0,0 +1,62 @@
+/*
+A linked list of length n is given such that each node contains an additional random pointer, which could point to any node in the list, or null.
+
+Construct a deep copy of the list. The deep copy should consist of exactly n brand new nodes, where each new node has its value set to the value of its corresponding original node. Both the next and random pointer of the new nodes should point to new nodes in the copied list such that the pointers in the original list and copied list represent the same list state. None of the pointers in the new list should point to nodes in the original list.
+
+For example, if there are two nodes X and Y in the original list, where X.random --> Y, then for the corresponding two nodes x and y in the copied list, x.random --> y.
+
+Return the head of the copied linked list.
+
+The linked list is represented in the input/output as a list of n nodes. Each node is represented as a pair of [val, random_index] where:
+
+val: an integer representing Node.val
+random_index: the index of the node (range from 0 to n-1) that the random pointer points to, or null if it does not point to any node.
+Your code will only be given the head of the original linked list.
+
+LINK: https://leetcode.com/problems/copy-list-with-random-pointer/description/
+*/
+
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        // constraints say n can be 0
+        if(!head) return nullptr;
+
+        unordered_map<Node*, Node*> mp; // mapping original and copied nodes
+
+        Node* temp = head;
+        // just create copy nodes
+        while(temp){
+            mp[temp] = new Node(temp->val);
+            temp = temp->next;
+        }
+
+        temp = head;
+
+        // assign next and random
+        while(temp){
+            mp[temp]->next = mp[temp->next];
+            mp[temp]->random = mp[temp->random];
+            temp = temp->next;
+        }
+
+        return mp[head];
+    }
+};
