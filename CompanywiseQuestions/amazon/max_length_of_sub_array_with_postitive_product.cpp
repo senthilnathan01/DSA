688846b0c804e43a8405025c466414b8b6312833
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2026-02-14 07:47:48 +0530

    Update max_length_of_sub_array_with_postitive_product.cpp

--- CompanywiseQuestions/amazon/max_length_of_sub_array_with_postitive_product.cpp
+++ CompanywiseQuestions/amazon/max_length_of_sub_array_with_postitive_product.cpp
@@ -8,4 +8,45 @@ Return the maximum length of a subarray with positive product.
 LINK: https://leetcode.com/problems/maximum-length-of-subarray-with-positive-product/description/
 */
 
+class Solution {
+public:
+    int getMaxLen(vector<int>& nums) {
+        // track start and end
+        // reset when passed through zero
+        // track sign by counting the number of negatives
 
+        int ans = 0;
+        int start = 0;
+        int n = nums.size();
+
+        while(start < n){
+            // skip zeroes
+            while(start < n && nums[start]==0) start++;
+            
+            // reached the end?
+            if(start==n) break;
+
+            // ready to start counting?
+            int end = start;
+            int num_negs = 0;
+            int firstNeg = -1;
+            int lastNeg = -1;
+            while(end < n && nums[end]!=0){
+                if(nums[end]<0){
+                    num_negs++;
+                    if(firstNeg==-1) firstNeg=end;
+                    lastNeg = end;
+                }
+                end++;
+            }
+
+            if(num_negs%2==0) ans = max(ans, end-start);
+            else{
+                ans = max(ans, end-1 -firstNeg);
+                ans = max(lastNeg-start, ans);
+            }
+            start = end;
+        }
+        return ans;
+    }
+};
