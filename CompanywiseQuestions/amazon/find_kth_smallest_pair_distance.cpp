ddefb109fb5a9e86c531871bc760204a28503abf
Senthilnathan T <157115274+senthilnathan01@users.noreply.github.com>
2026-02-17 14:57:01 +0530

    Create find_kth_smallest_pair_distance.cpp

---
+++ CompanywiseQuestions/amazon/find_kth_smallest_pair_distance.cpp
@@ -0,0 +1,29 @@
+/*
+The distance of a pair of integers a and b is defined as the absolute difference between a and b.
+
+Given an integer array nums and an integer k, return the kth smallest distance among all the pairs nums[i] and nums[j] where 0 <= i < j < nums.length.
+
+LINK: https://leetcode.com/problems/find-k-th-smallest-pair-distance/description/ 
+** Hard
+*/
+
+// Brute Force Approach
+/*
+Is just finding all the pair distance
+Storing it in a list
+Sorting them
+Returning the value at k-1 index
+
+No of pairs = O(n^2)
+TC = O(n^2) for finding pair distance + O(2* n^2 logn) for sorting = Overall O(n^2 logn)
+SC = O(n^2)
+*/
+
+// Optimal solution
+// Binary Search
+/*
+
+*/
+
+
+
